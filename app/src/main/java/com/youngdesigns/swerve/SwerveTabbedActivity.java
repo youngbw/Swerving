f7e6c306fc623a792f1905bce87340de908fbf90
youngbw <Athero23@gmail.com>
2015-04-19 20:54:59 -0700

    Fragment not working, crashes on commit();

--- app/src/main/java/com/youngdesigns/swerve/SwerveTabbedActivity.java
+++ app/src/main/java/com/youngdesigns/swerve/SwerveTabbedActivity.java
@@ -1,17 +1,32 @@
 package com.youngdesigns.swerve;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class SwerveTabbedActivity extends ActionBarActivity {
+
+
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swerve_tabbed);
+        manager = getFragmentManager();
+
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new FeedFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
     }
 
 
@@ -35,5 +50,14 @@ public class SwerveTabbedActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void feedClicked(View view) {
+        Fragment frag = manager.findFragmentById(R.id.fragment_container);
+        if (!(frag instanceof FeedFragment)) {
+            FeedFragment fragment = new FeedFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
     }
 }
